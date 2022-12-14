15264df32189292ced9d63781f0bcb0dba512b42
temuuderchik <narangereltemuuder@icloud.com>
2022-12-14 09:18:22 +0800

    okok

--- src/main/java/Admin/Login.java
+++ src/main/java/Admin/Login.java
@@ -45,8 +45,8 @@ public class Login extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Temuuder\\OneDrive\\Desktop\\POS\\POS-System\\src\\main\\java\\User\\5087579.png")); // NOI18N
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 20, -1, 82));
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\asus\\Downloads\\5087579 (1).png")); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 10, 90, 110));
 
         uname.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 2, 0, new java.awt.Color(0, 0, 0)));
         jPanel1.add(uname, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 160, 125, -1));
@@ -60,7 +60,7 @@ public class Login extends javax.swing.JFrame {
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 200, -1, -1));
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jButton1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Temuuder\\Downloads\\unlock (2).png")); // NOI18N
+        jButton1.setIcon(new javax.swing.ImageIcon("C:\\Users\\asus\\OneDrive\\Documents\\NetBeansProjects\\POS-System\\src\\main\\java\\User\\unlock (2).png")); // NOI18N
         jButton1.setText("Login");
         jButton1.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 0, 0, new java.awt.Color(0, 0, 0)));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
